d5cba8bd6368ffdb5b88722dffa2b6858dfeb6ad
phamthinhutduy <20130241@st.hcmuaf.edu.vn>
2024-06-03 00:05:55 +0700

    app bao chua hoan thien

--- src/Components/NewsBoard.tsx
+++ src/Components/NewsBoard.tsx
@@ -1,16 +1,6 @@
-import React, {useEffect, useState} from 'react';
 import NewsItem from "./NewsItem";
 
 function NewsBoard() {
-
-    const [articles, setArticles] = useState([]);
-
-    useEffect(() =>{
-        let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY'
-        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles()));
-
-
-    },[])
 
     return (
         <div>
